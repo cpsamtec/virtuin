b1f75e69a25bc96cc016ccca7f19a48dc0aaf2eb
Nathan Page <nathan.page@samtec.com>
2019-02-13 16:51:30 -0500

    Collapsable Task Groups

--- packages/virtuingui2/app/components/TaskGroupList/TaskGroupList.style.js
+++ packages/virtuingui2/app/components/TaskGroupList/TaskGroupList.style.js
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import MuiListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
 export const ListItem = styled(MuiListItem, {shouldForwardProp: prop => prop !== 'active'})`
   background: ${props => props.active ? '#394249' : null };
@@ -14,3 +15,21 @@ export const ListItemSecondary = styled(Typography)`
   margin-top: 8px;
   line-height: 1.4;
 `;
+
+export const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Line = styled.div`
+  margin: 0 15px;
+  flex-grow: 1;
+  border-bottom: solid 1px #555;
+`;
+
+export const ToggleButton = styled(IconButton)`
+  width: 36px;
+  height: 36px;
+  padding: 0;
+`
