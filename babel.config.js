eca3a9488fab2ae274e03ab7ce4a0c266d6beeae
Chris Page <chris.page@samtec.com>
2019-04-12 17:51:48 -0400

    correct packages paths

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,20 @@
 export default {
     overrides: [
         {
-            test: 'virtuingui2',
-            extends: 'virtuingui2/babel.config.js'
+            test: 'packages/virtuingui2',
+            extends: 'packages/virtuingui2/babel.config.js'
         },
         {
-            test: 'virtuintaskdispatcher',
-            extends: 'virtuintaskdispatcher/babel.config.js'
+            test: 'packages/virtuintaskdispatcher',
+            extends: 'packages/virtuintaskdispatcher/babel.config.js'
         },
         {
-            test: 'task-rest-service',
-            extends: 'task-rest-service/babel.config.js'
+            test: 'packages/task-rest-service',
+            extends: 'packages/task-rest-service/babel.config.js'
         },
         {
-            test: 'cli',
-            extends: 'cli/babel.config.js'
+            test: 'packages/cli',
+            extends: 'packages/cli/babel.config.js'
         }    
 ]
 };
